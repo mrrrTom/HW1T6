1a2ab1a1c7705a3e959b62189a48dc17762478b4
Daniil Gorbunov <dagoroff@gmail.com>
2023-01-15 23:38:18 +0600

    Added solution for the task 6

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Welcome to the IsEven program!");
+Console.WriteLine("Please, insert your number:");
+var input = Console.ReadLine();
+var number = default(int);
+if (int.TryParse(input, out number))
+{
+    Console.WriteLine( number % 2 == 0 ? "да" : "нет");
+}
+else
+{
+    Console.WriteLine("Sorry, program could not handle inserted value... Bye!");
+}
